94c053868b15b63de7e538c5f485a93f883be70b
Ade-lilly <adebalanced7@gmail.com>
2023-05-07 19:01:20 +0100

    ammendment made in js

--- women-app.js
+++ women-app.js
@@ -1,23 +1,32 @@
 let Form = document.getElementById('form');
 Form.addEventListener('submit', function(event){
     event.preventDefault();
+    
+    let fullName = document.getElementById('fname')
+    console.log(`My name is ${fullName.value}`)
 
     let Location = document.getElementById('location');
     console.log(`My location is ${Location.value}`)
 
     let  Bust= document.getElementById('bust');
-    console.log(`My name is ${Bust.value}`)
+    console.log(`My bust is ${Bust.value}`)
+
     let Hip = document.getElementById('hip');
-    console.log(`My name is ${Hip.value}`)
+    console.log(`My hip is ${Hip.value}`)
+
     let Shoulder = document.getElementById('shoulder');
-    console.log(`My name is ${Shoulder.value}`)
+    console.log(`My shoulder is ${Shoulder.value}`)
+
     let Apex = document.getElementById('apex');
-    console.log(`My name is ${Apex.value}`)
+    console.log(`My apex is ${Apex.value}`)
+
     let Neck = document.getElementById('neck');
-    console.log(`My name is ${Neck.value}`)
+    console.log(`My neck is ${Neck.value}`)
+
     let Waist = document.getElementById('waist');
-    console.log(`My name is ${Waist.value}`)
+    console.log(`My waist is ${Waist.value}`)
+
     let Floor = document.getElementById('floor');
-    console.log(`My name is ${Floor.value}`)
+    console.log(`My floor is ${Floor.value}`)
   
 })
